3207f13bbce5587dc8b37a9b25f0eb084eccd966
renshufan <976590628@qq.com>
2019-04-27 14:08:34 +0800

    添加站点

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,11 +1,8 @@
 <template>
     <div>
       <div>{{userLoginInfo}}</div>
-<<<<<<< HEAD
-      <div>站點名稱1{{position}}</div>
-=======
+      <div>站点:{{position}}</div>
       <div>{{position}}</div>
->>>>>>> fd985e3b6ccf19b03abeb74a653d7312dc49a9b3
       <div>{{siteId}}</div>
       <div>{{siteName}}</div>
       <div>{{account}}</div>
