e8f550787ae8a4f181c2ff3128c98943fa827ff3
renshufan <976590628@qq.com>
2019-11-08 17:48:05 +0800

    文件数据改变3

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,7 @@
       <a href="javascript:;" @click="first">通过methods方法执行 actions动作(异步)</a>
     </div>
     <div>
-      <a href="javascript:;" @click="$router.push('/card')">跳转card组件,进行展示2</a>
+      <a href="javascript:;" @click="$router.push('/card')">跳转card组件,进行展示3</a>
     </div>
   </div>
 </template>
