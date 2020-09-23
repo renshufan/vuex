4381d301e2351f69b4a323563a41d1a6583a3fdb
renshufan <976590628@qq.com>
2020-09-23 16:34:17 +0800

    123123

--- src/view/fruit/banana/banana.vue
+++ src/view/fruit/banana/banana.vue
@@ -5,19 +5,40 @@
 </template>
 
 <script>
-  export default {
-    name: "banana",
-    methods: {
-      doClick () {
-        this.$router.push({ name: 'carrot' })
-      }
+const path = require("path");
+
+export default {
+  name: "banana",
+  created() {
+    console.log(path.resolve("package.json"), "resolve");
+    console.log(path.basename("package.json"), "basename");
+    console.log(path.dirname("package.json"), "dirname");
+    console.log(path.extname("package.json"), "extname");
+    console.log(path.join("/", "package.json"), "join");
+    console.log(path.basename("package.json", ".json"), ".json");
+    console.log(path.isAbsolute("/package.json"), "isAbsolute");
+    console.log(path.isAbsolute("package.json"), "isAbsolute");
+    // console.log(path.parse("/app.js"), "parse");
+  },
+  mounted() {},
+  methods: {
+    doClick() {
+      this.$router.push({ name: "carrot" });
     },
-    created() {
-      this.$router.replace(this.$route.name, this.$route.path,)
+    do() {
+      new Promise((resolve, reject) => {
+        reject("这是一个错误");
+      })
+        .then(res => {
+          console.log(res, "resolve");
+        })
+        .catch(error => {
+          console.log(error, "失败");
+        });
     }
   }
+};
 </script>
 
 <style scoped>
-
 </style>
